53e6b47f23c19b13a1519e89185043cf8b83a513
arbanQureshi <102940891+arbanQureshi@users.noreply.github.com>
2022-10-18 12:37:14 +0530

    button text changed

--- lib/main.dart
+++ lib/main.dart
@@ -74,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     baba.name=_name.text;
                     baba.password=_password.text;
                     fetch(ty: baba.toJson());
-                  }, child:Text('preaa') ,),
+                  }, child:Text('Press Me') ,),
                 ],
               ),
             ),
